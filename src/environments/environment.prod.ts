31933b7fdf7d3f6363ff732af456678179e199fe
nSy0 <rignacio871@gmail.com>
2024-11-26 23:41:48 -0300

    android

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyA8dSglvPkFFLET7r8eQWok8JcVVSX7_OA",
+    authDomain: "trellok-1502.firebaseapp.com",
+    projectId: "trellok-1502",
+    storageBucket: "trellok-1502.firebasestorage.app",
+    messagingSenderId: "896738421806",
+    appId: "1:896738421806:web:681d66a0315a89102ac3bf",
+    measurementId: "G-XNTQT61PYN"
+  }
 };
